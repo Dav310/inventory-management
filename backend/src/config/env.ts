4c83b5cb43d4ac87e4822d386128cba3b545903f
Rahul <iamdavid31@gmail.com>
2026-07-10 22:04:40 +0530

    RB

--- backend/src/config/env.ts
+++ backend/src/config/env.ts
@@ -5,7 +5,7 @@ dotenv.config();
 export const env = {
   PORT: Number(process.env.PORT) || 5000,
 
-  DB_HOST: process.env.DB_HOST || "db.nfskejmficrxcqpnbguq.supabase.co",
+  DB_HOST: process.env.DB_HOST || "localhost",
   DB_PORT: Number(process.env.DB_PORT) || 5432,
   DB_USER: process.env.DB_USER || "postgres",
   DB_PASSWORD: process.env.DB_PASSWORD || "",
