8148d75e13674a9c435d649df6b71fc5a80ede62
Rahul <iamdavid31@gmail.com>
2026-07-10 23:33:23 +0530

    Update start.sh

--- redpanda/start.sh
+++ redpanda/start.sh
@@ -1,9 +1,10 @@
+#!/bin/sh
 
 exec redpanda start \
   --overprovisioned \
   --smp=1 \
   --memory=512M \
   --reserve-memory=0M \
-  --node-id=0 \
-  --kafka-addr=PLAINTEXT://0.0.0.0:29092,OUTSIDE://0.0.0.0:9092 \
-  --advertise-kafka-addr=PLAINTEXT://redpanda:29092,OUTSIDE://redpanda:9092
+  --node-id 0 \
+  --kafka-addr PLAINTEXT://0.0.0.0:29092,OUTSIDE://0.0.0.0:9092 \
+  --advertise-kafka-addr PLAINTEXT://redpanda:29092,OUTSIDE://redpanda:9092
