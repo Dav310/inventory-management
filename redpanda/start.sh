61f36e5a89040f6dfde83440bed2d4a7328e8c31
Rahul <iamdavid31@gmail.com>
2026-07-11 11:17:24 +0530

    Update start.sh

--- redpanda/start.sh
+++ redpanda/start.sh
@@ -4,4 +4,6 @@ exec rpk redpanda start \
   --overprovisioned \
   --smp 1 \
   --memory 512M \
-  --reserve-memory 0M
+  --reserve-memory 0M \
+  --kafka-addr PLAINTEXT://0.0.0.0:9092 \
+  --advertise-kafka-addr PLAINTEXT://valiant-exploration.railway.internal:9092
